68265841a05e376fcfd3f5b66ef27cac87cb1d5d
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-10-12 07:19:41 +0300

    color-switcher-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+let colorChangeId = 0;
+
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
+refs.stop.disabled = true;
+refs.start.addEventListener('click', handleStart);
+refs.stop.addEventListener('click', handleStop);
+
+function handleStart() {
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+  const colorChange = setInterval(() => {
+    const randC = getRandomHexColor();
+    document.querySelector('body').style.backgroundColor = randC;
+  }, 1000);
+  colorChangeId = colorChange;
+}
+
+function handleStop() {
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
+  clearInterval(colorChangeId);
+}
